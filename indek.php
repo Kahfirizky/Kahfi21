9d5fd556001f0d142a233e8ff5a434f0b94fb561
unknown <kahfirizky>
2024-07-05 20:14:34 +0700

    bagian kata kata

---
+++ indek.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Home Content</title>
+</head>
+<body>
+    <center><h1>Home Page</h1></center>
+    <!-- <p>Welcome to the Home page of your Dashboard. This content is displayed from a separate HTML file.</p> -->
+
+
+    
+</body>
+</html>
